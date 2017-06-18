65812095a0744dd40dbd90eb2400cfb051fc1f51
Sebastian Henriksen <sebastian@henriksen.io>
2017-06-18 09:37:30 +0200

    Updating to use engine version 4.16

--- Source/BPSortingEditor.Target.cs
+++ Source/BPSortingEditor.Target.cs
@@ -8,18 +8,7 @@ public class BPSortingEditorTarget : TargetRules
 	public BPSortingEditorTarget(TargetInfo Target)
 	{
 		Type = TargetType.Editor;
-	}
 
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "BPSorting" } );
-	}
+		ExtraModuleNames.AddRange( new string[] { "BPSorting" } );
+    }
 }
